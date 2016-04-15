67b34a3a02ff96ad2d3eed32d4b6b42c9b821956
CatherineGrillo <cgrillo2727@gmail.com>
2016-04-15 16:07:51 -0400

    More features to Attributes and CreateChar classes

--- Attributes.java
+++ Attributes.java
@@ -1,39 +1,64 @@
 /**
  * @author Catherine Grillo
  * @date created 3/09/2016
- *@date updated 4/09/2016
- *attribute class to set the strength, wisdom, dexterity,
- *constitution, and intelligence of a character.
+ *@date updated 4/15/2016
+ *attribute class to hold and set all attributes of a character.
  */
 
 import java.util.Objects;
 
 public class Attributes {
+	//name of character
+	String name = new String();
 	//races: human, lizard, elf
-	private String race = new String;
+	String race = new String();
 	//type is character class: warrior, ranger, wizard
-	private String type = new String;
-	//strength used for melee attacks
-	private int str;
-	//dexterity used for ranged attacks
-	private int dex;
+	String type = new String();
+	
+	//character starts off not advanced. After first encounter, they become advanced
+	boolean isAdvanced = false;
+
+	
+	//strength used for warrior attacks
+	int str;
+	//dexterity used for ranger attacks
+	int dex;
+	//wisdom used for wizard attacks
+	int wis;
 	//constitution used for health
-	private int con;
-	//wisdom used for magic attacks
-	private int wis;
+	int con;
+	
+	//weapon type
+	String weapon = new String();
+	//weapon bonus based on advancement
+	int wBonus = 5;
+	
 	//attack points
-	private int ap;
+	int ap;
 	//defense (reduce damage done by a percentage)
-	private double defense;
+	double defense;
 	//accuracy (determines character's chance to hit) (percentage)
-	private double acc;
+	double acc;
+	
 	//total health (doesn't change)
-	private int totalhp = 10+con;
+	int totalhp = 10+con;
 	//current health (starts = to totalhp, reduced by combat)
-	private int currenthp = totalhp;
+	int currenthp = totalhp;
 	
 	
 	//methods to alter attributes
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setRace(String race) {
+		this.race = race;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 	public void setStr(int str) {
 		this.str = str;
 	}
@@ -50,19 +75,81 @@ public class Attributes {
 		this.wis = wis;
 	}
 	
-	//calculate attack points based on type
-	public void setAttack(String type) {
+	//set weapon type based on character type
+	public void setWeapon(String type) {
 		if (Objects.equals(type,"warrior")) {
-			//warriors have medium attack power (but will have high defense and low accuracy)
-			this.ap = 15+str;
+			weapon = "battle axe";
 		}
 		else if (Objects.equals(type,"wizard")) {
-			//wizards have high attack power (but will have low defense and medium accuracy)
-			this.ap = 17+wis;
+			weapon = "staff";
 		}
-		else if (Objects.equals(type,"ranger")) {
-			//rangers have low attack power (but will have high accuracy and medium defense)
-			this.ap = 12+dex;
+		else {
+			weapon = "longbow";
 		}
 	}
+	
+	//increase weapon bonus after advancement
+	public void upWBonus() {
+		wBonus = 10;
+	}
+	
+	//calculate attack points based on type
+	public void setAp(String type) {
+		if (Objects.equals(type,"warrior")) {
+			//warriors have medium attack power
+			this.ap = 15+str+wBonus;
+		}
+		else if (Objects.equals(type,"wizard")) {
+			//wizards have high attack power
+			this.ap = 17+wis+wBonus;
+		}
+		else {
+			//rangers have low attack power
+			this.ap = 12+dex+wBonus;
+		}
+	}
+	
+	//calculate defense based on type (and constitution)
+		public void setDefense(String type) {
+			if (Objects.equals(type,"warrior")) {
+				//warriors have high defense
+				this.defense = (17+con/2.0)/100;
+			}
+			else if (Objects.equals(type,"wizard")) {
+				//wizards have low defense
+				this.defense = (12+con/2.0)/100;
+			}
+			else  {
+				//rangers have medium
+				this.defense = (15+con/2.0)/100;
+			}
+		}
+		
+		//calculate accuracy based on type
+		public void setAcc(String type) {
+			if (Objects.equals(type,"warrior")) {
+				//warriors have low accuracy
+				this.acc = 92/100.0;
+			}
+			else if (Objects.equals(type,"wizard")) {
+				//wizards have medium accuracy
+				this.acc = 95/100.0;
+			}
+			else  {
+				//rangers have high accuracy
+				this.acc = 97/100.0;
+			}
+		}
+		
+		//reduce character health
+		public void loseHealth(int damage) {
+			this.currenthp -= damage;
+			if (this.currenthp <=0) {
+				System.out.println(this.name+" has died");
+				this.currenthp = 0;
+			}
+			else {
+				System.out.println(this.name+" has "+this.currenthp+" health left");
+			}
+		}
 }
--- BattleSimulation.java
+++ BattleSimulation.java
@@ -1,17 +1,20 @@
 /**
  * 
- * @author Catherine Grillo
+ *@author Catherine Grillo
  *@date created 03/17/2016
- *@date updated 03/18/2016
- *CSC 218.01 Midterm
+ *@date updated 04/15/2016
+ *Originally CSC 218.01 Midterm. Now just test program
  *Allow user to create 2 characters and have them battle
+ *
+ *Current battle simulation works but does not incorporate all aspects of the character. This is simply a leftover program that,
+ *with a little tweaking, allowed me to test out many changes I made to the Attributes class and the CreateChar class
  */
 
 //import scanner for user input
 import java.util.Objects;
 import java.util.Scanner;
 
-public class CharacterCreation {
+public class BattleSimulation {
 
 	public static void main(String[] args) throws java.io.IOException{
 		CreateChar chars[] = new CreateChar[2];
@@ -20,25 +23,30 @@ public class CharacterCreation {
 		try {
 			//loop until user has made 2 characters
 			for (int i = 0; i<2; i++) {
-				//get character class
-				System.out.println("Would you like your character to be a sorcerer, a fighter, or a ranger?");
+				
+				//get character type
+				System.out.println("Would you like your character to be a wizard, a warrior, or a ranger?");
 				String type = user_input.next();
-				//make sure it it is a valid class
-				while (!Objects.equals(type, "sorcerer") && !Objects.equals(type,  "fighter") && !Objects.equals(type,  "ranger")){
-					System.out.println("Oops, I didn't catch that. Please enter 'sorcerer', 'fighter', or 'ranger'.");
+				//make sure it it is a valid type
+				while (!Objects.equals(type, "wizard") && !Objects.equals(type,  "warrior") && !Objects.equals(type,  "ranger")){
+					System.out.println("Oops, I didn't catch that. Please enter 'wizard', 'warrior', or 'ranger'.");
 					type = user_input.next();
 				}
 				
 				//get character race.
-				System.out.println("What would you like your character's race to be?");
+				System.out.println("What would you like your character to be a lizard, a human, or an elf?");
 				String race = user_input.next();
+				while (!Objects.equals(race, "lizard") && !Objects.equals(race, "human") && !Objects.equals(race, "elf")){
+					System.out.println("Oops, I didn't catch that. Please enter 'lizard', 'human', or 'elf'.");
+					race = user_input.next();
+				}
 				
 				//get character name
 				System.out.println("What would you like to name your character?");
 				String name = user_input.next();
 				
 				//initialize character
-				chars[i] = new CreateChar(type, race, name);
+				chars[i] = new CreateChar(name, race, type, 20, 15, 12, 20);
 				chars[i].printchar();
 			}
 			
@@ -57,7 +65,7 @@ public class CharacterCreation {
 			System.out.println(chars[f].name+" is now fighting "+chars[s].name);
 			
 			while (true) {
-				if (Objects.equals(chars[f].type, "fighter")) {
+				if (Objects.equals(chars[f].type, "warrior")) {
 					//fighter closes the distance
 					if (dist>0) {
 						System.out.println(chars[f].name + " runs up to "+chars[s].name);
@@ -65,7 +73,7 @@ public class CharacterCreation {
 					}
 					//hit opponent
 					System.out.println(chars[f].name+ " swings their "+chars[f].weapon+" at "+chars[s].name);
-					chars[s].loseHealth(chars[f].melee);
+					chars[s].loseHealth(chars[f].ap);
 				}//end fighter turn
 				
 				else if (Objects.equals(chars[f].type, "ranger")) {
@@ -76,10 +84,10 @@ public class CharacterCreation {
 					}
 					//hit opponent
 					System.out.println(chars[f].name+ " shoots their "+chars[f].weapon+" at "+chars[s].name);
-					chars[s].loseHealth(chars[f].ranged);
+					chars[s].loseHealth(chars[f].ap);
 				}//end ranger turn
 				
-				else if (Objects.equals(chars[f].type, "sorcerer")) {
+				else if (Objects.equals(chars[f].type, "wizard")) {
 					//sorcerer backs up
 					if (dist==0) {
 						System.out.println(chars[f].name + " backs away from "+chars[s].name);
@@ -87,16 +95,16 @@ public class CharacterCreation {
 					}
 					//hit opponent
 					System.out.println(chars[f].name+ " uses their "+chars[f].weapon+" to cast a spell on "+chars[s].name);
-					chars[s].loseHealth(chars[f].spell);
+					chars[s].loseHealth(chars[f].ap);
 				}//end sorcerer turn
 				
 				
-				if (chars[s].healthC==0) {
+				if (chars[s].currenthp==0) {
 					System.out.println(chars[f].name+" has defeated "+chars[s].name);
 					break;
 				}//end if chars[s] dies
 				
-				else if (chars[f].healthC==0) {
+				else if (chars[f].currenthp==0) {
 					System.out.println(chars[s].name+" has defeated "+chars[f].name);
 					break;
 				}//end if chars[f] dies
@@ -108,8 +116,8 @@ public class CharacterCreation {
 			}//end while
 			
 			//reset current health of both characters to  total health
-			chars[f].healthC = chars[f].healthT;
-			chars[s].healthC = chars[s].healthT;
+			chars[f].currenthp = chars[f].totalhp;
+			chars[s].currenthp = chars[s].totalhp;
 		}//end try
 		
 		//close scanner
