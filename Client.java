83409f803b6d509d3d378ec010b6e161e7fb5548
CatherineGrillo <cgrillo2727@gmail.com>
2016-04-18 19:23:26 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ActiveSession.java
    #	Character.java
    #	Client.java
    #	CombatManager.java
    #	QuestManager.java
    #	SessionManager.java
    
    Merge remote-tracking branch 'origin/master'
    
    #
    Conflicts:
    #	ActiveSession.java
    #	Character.java
    #	Client.java
    #	CombatManager.java
    #	QuestManager.java
    #	README.txt
    #	SessionManager.java
    
    removed
    .class

---
+++ Client.java
@@ -0,0 +1,9 @@
+/*
+ * @author Ian Gompers
+ * @date created Fri, Apr 15, 2016  2:36:34 AM
+ * @last modified Fri, Apr 15, 2016  2:57:33 AM
+ * @description This class facilitates a role playing game.
+ */
+public class Client {
+
+} // end Client class
---
+++ CombatManager.java
@@ -0,0 +1,21 @@
+/*
+ * @author Ian Gompers
+ * @date created Fri, Apr 15, 2016  2:36:34 AM
+ * @last modified Fri, Apr 15, 2016  2:57:33 AM
+ * @description This class facilitates combat.
+ */
+public class CombatManager {
+	
+	void initiateCombatSession() {
+		// prompt user with appropriate choices
+	}
+	
+	void attack() {
+		// calculate damage points
+		
+		// adjust damage points for accuracy semi-randomly
+		
+		// subtract damage points from enemy health
+	}
+	
+} // end CombatManager class
