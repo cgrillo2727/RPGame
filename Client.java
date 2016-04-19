1eb09197514c4e6590320ed8bf3c0410cb415f83
jtalexander2325 <jamiiis2325@aol.com>
2016-04-18 20:06:55 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,9 +1,33 @@
 /*
- * @author Ian Gompers
- * @date created Fri, Apr 15, 2016  2:36:34 AM
- * @last modified Fri, Apr 15, 2016  2:57:33 AM
- * @description This class facilitates a role playing game.
+Class created by James
+4/18/2016
  */
-public class Client {
 
-} // end Client class
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Client extends Application
+{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml")); //temporary root
+        
+        //Title
+        primaryStage.setTitle("Trogdor the Burninator");
+        
+        //Size
+        primaryStage.setScene(new Scene(root, 750, 650));
+        primaryStage.show();
+    }
+
+    //This is where we'll call all classes and modules
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
