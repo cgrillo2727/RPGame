cfb23322070af488f19d0ddb81d7d74c3076c253
Shawn-L <swood538@yahoo.com>
2016-04-28 10:01:33 -0400

    Added a class for the two NPCs

--- src/RPG/Game/npcCharacterCreator.java
+++ src/RPG/Game/npcCharacterCreator.java
@@ -4,5 +4,5 @@ import java.lang.String;
  * Created by Shawn-L on 4/28/2016.
  */
 public class npcCharacterCreator {
-    Character Shopkeeper = new Character(String type, String name, String race, int constitution, int strength, int dexterity, int wisdom);
+
 }
