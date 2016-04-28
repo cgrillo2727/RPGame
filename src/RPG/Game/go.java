d6d56b0e5e724619a63910a2bb45a7179f224978
grah3367 <grah3367@chawk.cecil.edu>
2016-04-28 16:50:39 -0400

    go launcher updated with the ability to tell if program is ran from a console window and a jar file. This allows for it to act as a self-launcher and double clicking on a compiled jar will open a windows command prompt and run the game. (it is self-starting).

--- src/RPG/Game/go.java
+++ src/RPG/Game/go.java
@@ -1,5 +1,8 @@
 package RPG.Game;
+import java.io.Console;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 /*
  * Created by Shawn-L on 4/25/2016.
@@ -8,13 +11,36 @@ public class go {
 
     // create ONE scanner object and have other files get it.
     static final Scanner io = new Scanner(System.in);
+    static boolean isJar = false;
 
     public static void main(String args[])throws InterruptedException{
 
-        QuestManager.beginCredits();
-        CreateCharacter.createCharacter();
-        QuestManager.sequenceA1();
-        QuestManager.sequenceC4();
+        Console console = System.console();
+
+        if( Character.class.getResource("Character.class").toString().contains("jar") ) {
+            isJar = true;
+        }
+
+        if(console == null && isJar) {
+            try {
+                Runtime rt = Runtime.getRuntime();
+                //String game_jar_dir = "C:\\Users\\Dan\\IdeaProjects\\RPGfinalProject\\out\\artifacts\\RPG_Game_jar";
+                String game_jar_dir = System.getProperty("user.dir");
+                rt.exec("cmd.exe /c cd \"" + game_jar_dir + "\" & start \"<RPG Game Final Project>\" /REALTIME cmd.exe /k \"java -cp .;RPG-Game.jar; RPG.Game.go \"");
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else { // we are in a windows cmd
+
+            QuestManager.beginCredits();
+            CreateCharacter.createCharacter();
+            QuestManager.sequenceA1();
+            QuestManager.sequenceC4();
+
+            System.out.println("it wasent loaded from the console!");
+            System.out.println(console);
+        }
 
 
         io.close(); // only close the scanner object when you're done getting all input.
